a5a95222edcb771ff32a6c5e0579f29a13bbf7fd
daniello7 <daniellopezolmos7@gmail.com>
2025-04-17 13:51:35 +0200

    Creada migración Posts

---
+++ database/migrations/2025_04_17_110528_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('video_id')->constrained();
+            $table->foreignId('podcasts_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('tag_id')->constrained();
+            $table->string('title');
+            $table->string('slug')->default('');
+            $table->string('body');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};

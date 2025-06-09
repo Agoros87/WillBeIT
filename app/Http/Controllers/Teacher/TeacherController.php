aa8511cfc5531c26d9bc035d48c9e0cebe665565
daniello7 <daniellopezolmos7@gmail.com>
2025-06-09 18:24:13 +0200

    Correcciones a la hora de cargar y navegar con el public layout y scroll, refactorización de partials.navigation

---
+++ app/Http/Controllers/Teacher/TeacherController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Teacher;
+
+use App\Http\Controllers\Controller;
+
+class TeacherController extends Controller{
+    public function index()
+    {
+        //
+    }
+}

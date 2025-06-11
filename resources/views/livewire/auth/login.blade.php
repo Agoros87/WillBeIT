ea40bb8a05738e87a4434694ac4fe9917aa5881e
IVANCAMACHO\diskoten <ivancalp9@gmail.com>
2025-06-11 16:23:46 +0200

    Añadimos ajuste de vista invitado y traducciones y logos

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -110,12 +110,13 @@ new #[Layout('components.layouts.auth')] class extends Component {
     }
 }; ?>
 <div class="flex flex-col gap-6">
+
     <x-auth-header :title="__('Log in to your account')" :description="__('Enter your email and password below to log in')"/>
     <!-- Session Status -->
     <x-auth-session-status class="text-center" :status="session('status')"/>
     <form wire:submit="login" class="flex flex-col gap-6">
         <!-- Email Address -->
-        <flux:input wire:model="email" :label="__('Email address')" type="email" required autofocus autocomplete="email" placeholder="email@example.com"/>
+        <flux:input wire:model="email" :label="__('Email')" type="email" required autofocus autocomplete="email" placeholder="email@example.com"/>
         <!-- Password -->
         <div class="relative">
             <flux:input wire:model="password" :label="__('Password')" type="password" required autocomplete="current-password" :placeholder="__('Password')"/>
